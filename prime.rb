57141dcc00916c4af4eec9808e05731bf696a907
Renee Nordholm <reneenordholm@gmail.com>
2019-04-28 23:48:23 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,5 +1,9 @@
 def prime?(number)
-  if number.each do { |num| num % num == 0}
+
+  if number.each do { |num| num % num == 0 }
     true
-  elseif number.each do { |num| 
+  elseif number.each do { |num| -num % -num == 0 }
+    true
+  else
+    false
 end
