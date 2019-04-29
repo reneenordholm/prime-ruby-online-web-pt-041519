f15c6def9d68dbd9c298eaf9b2121ed2add699e6
Renee Nordholm <reneenordholm@gmail.com>
2019-04-29 00:02:38 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,10 @@
-# Add  code here!
+def prime?(num)
+  return false if num <= 0
+  return false if num == 1
+  (2..num/2).each do |i|
+    if num % i == 0
+      return false
+    end
+  end
+  true
+end
